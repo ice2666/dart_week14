3fcede9d38e2a1d79ae2fa4c277be4b9aff4d625
rathapoom260448 <rathaphumiwicitrsmbati.890@gmail.com>
2022-08-15 11:26:30 +0700

    first commit

---
+++ bin/dart_week14.dart
@@ -0,0 +1,8 @@
+void main () {
+  int sum = add(10,5,2);
+  print(sum);
+}
+
+int add(int a,int b, int c) {
+  return a - b - c;
+}
---
+++ bin/function101.dart
@@ -0,0 +1,8 @@
+void main () {
+  add();
+}
+add(){
+  int a = 10;
+  int b = 5;
+  print(a+b);
+}
